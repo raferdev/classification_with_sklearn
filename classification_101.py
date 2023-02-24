de106b4a184fe2a65c6734dabb7ee97a7c7e1389
Rafael <rafinha_fern@hotmail.com>
2023-02-24 03:51:49 -0300

    feat: add linearSVC

--- classification_101.py
+++ classification_101.py
@@ -1,3 +1,4 @@
+from sklearn.svm import LinearSVC
 
 pig1 = [0, 1, 0]
 pig2 = [0, 1, 1]
@@ -10,3 +11,6 @@ dog3 = [1, 1, 1]
 train_x = [pig1, pig2, pig3, dog1, dog2, dog3]
 
 train_y = [1, 1, 1, 0, 0, 0]
+
+model = LinearSVC()
+model.fit(train_x, train_y)
